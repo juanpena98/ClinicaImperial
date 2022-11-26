63d3537fb650819fec732f60d785a0a2b88dd5b8
renelfb <renelfb@gmail.com>
2022-11-26 14:41:39 -0500

    trabajo

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
       <Route exact path="/home"element = {<Home/>}/>
       <Route exact path="/Certificados" element = {<Certificados/>}/>
       <Route exact path="/Mensajes" element = {<Mensajes/>}/>
+      <Route exact path="/Mensajes" element = {<Inicio/>}/>
       
       
       </Routes>
